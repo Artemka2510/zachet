64e5c0489a5295ec1bb331654dee38574050d6e3
Artemka2510 <artem250204@mail.ru>
2024-12-21 23:40:03 +0300

    try3

--- mainWindow.py
+++ mainWindow.py
@@ -7,18 +7,18 @@ class MainWindow(QWidget):
         super().__init__(parent)
         self.setWindowTitle('Список материалов')
 
-        # Основной вертикальный layout
+
         vbox = QVBoxLayout()
 
-        # Горизонтальный layout для поиска и фильтрации
+
         hbox = QHBoxLayout()
 
-        # Поле для ввода поиска
+
         self.search_edit = QLineEdit(parent=self)
         self.search_edit.setPlaceholderText("Введите для поиска")
         hbox.addWidget(self.search_edit)
 
-        # Комбобокс для сортировки
+
         self.sort_cbox = QComboBox(parent=self)
         self.sort_cbox.addItems(["Сортировка", "Сортировать по дате", "Сортировать по количеству"])
         hbox.addWidget(self.sort_cbox)
