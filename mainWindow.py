de89c9272d8039b1a109fff4151f49c19361a133
Artemka2510 <119489875+Artemka2510@users.noreply.github.com>
2024-12-24 19:41:02 +0300

    Update mainWindow.py

--- mainWindow.py
+++ mainWindow.py
@@ -7,11 +7,8 @@ class MainWindow(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.setWindowTitle('Список материалов')
-
-        # Основной вертикальный layout
+        
         vbox = QVBoxLayout()
-
-        # Создаем горизонтальный layout для поиска и кнопок
         hbox = QHBoxLayout()
 
         # Поле для ввода поиска
@@ -30,39 +27,37 @@ class MainWindow(QWidget):
         # Добавляем горизонтальный layout в основной вертикальный layout
         vbox.addLayout(hbox)
 
-        # Таблица для отображения материалов
+ 
         self.table_widget = QTableWidget(self)
         vbox.addWidget(self.table_widget)
 
         self.setLayout(vbox)
 
-        # Подключаемся к базе данных и загружаем данные
+ 
         self.load_data()
 
     def load_data(self):
         try:
-            # Подключение к базе данных PostgreSQL
+            
             connection = psycopg2.connect(
                 dbname='zachet123',
-                user='postgres',  # замените на ваше имя пользователя
-                password='artem',  # замените на ваш пароль
-                host='localhost',  # или адрес вашего сервера базы данных
-                port='5432'  # стандартный порт PostgreSQL
+                user='postgres',  
+                password='artem', 
+                host='localhost',  
+                port='5432'  
             )
             cursor = connection.cursor()
 
-            # Получаем данные из таблицы materials
+
             cursor.execute("SELECT * FROM materials")
             rows = cursor.fetchall()
 
-            # Устанавливаем количество строк и столбцов в таблице
+
             self.table_widget.setRowCount(len(rows))
             self.table_widget.setColumnCount(3)  # id, title, quantity
 
-            # Устанавливаем заголовки столбцов
             self.table_widget.setHorizontalHeaderLabels(['ID', 'Title', 'Quantity'])
 
-            # Заполняем таблицу данными
             for row_index, row_data in enumerate(rows):
                 for column_index, item in enumerate(row_data):
                     self.table_widget.setItem(row_index, column_index, QTableWidgetItem(str(item)))
